2ac1f018ea6b9285f1e275c4f0ffaf4f238ab33c
Atrox493 <2577632081@qq.com>
2024-07-14 00:24:57 +0800

    整体结构拆分&分类数据拆分

--- Vue3Rabbit/src/views/Home/index.vue
+++ Vue3Rabbit/src/views/Home/index.vue
@@ -1,3 +1,17 @@
+<script setup>
+import HomeCategory from "./components/HomeCategory.vue";
+import HomeBanner from "./components/HomeBanner.vue";
+import HomeNew from "./components/HomeNew.vue";
+import HomeHot from "./components/HomeHot.vue";
+import HomeProduct from "./components/HomeProduct.vue";
+</script>
+
 <template>
-  <div>home页</div>
+  <div class="container">
+    <HomeCategory />
+    <HomeBanner />
+  </div>
+  <HomeNew />
+  <HomeHot />
+  <HomeProduct />
 </template>
