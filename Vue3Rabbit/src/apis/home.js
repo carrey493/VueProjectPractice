32ef73f8d3f86739a8b9c6b83fc5dd7d4aeef23a
Atrox493 <2577632081@qq.com>
2024-07-14 13:08:00 +0800

    轮播图组件实现

---
+++ Vue3Rabbit/src/apis/home.js
@@ -0,0 +1,8 @@
+import httpInstance from "@/utils/http";
+
+//获取banner
+export function getBannerAPI() {
+  return httpInstance({
+    url: "/home/banner",
+  });
+}
--- Vue3Rabbit/src/views/Home/components/HomeBanner.vue
+++ Vue3Rabbit/src/views/Home/components/HomeBanner.vue
@@ -1,5 +1,40 @@
-<script setup></script>
+<script setup>
+import { getBannerAPI } from "@/apis/home";
+import { ref, onMounted } from "vue";
+
+const bannerList = ref([]);
+const getBanner = async () => {
+  const res = await getBannerAPI();
+  bannerList.value = res.result;
+};
+
+onMounted(() => {
+  getBanner();
+});
+</script>
 
 <template>
-  <div></div>
+  <div class="home-banner">
+    <el-carousel height="500px">
+      <el-carousel-item v-for="item in bannerList" :key="item.id">
+        <img :src="item.imgUrl" alt="" />
+      </el-carousel-item>
+    </el-carousel>
+  </div>
 </template>
+
+<style scoped lang="scss">
+.home-banner {
+  width: 1240px;
+  height: 500px;
+  position: absolute;
+  left: 0;
+  top: 0;
+  z-index: 98;
+
+  img {
+    width: 100%;
+    height: 500px;
+  }
+}
+</style>
