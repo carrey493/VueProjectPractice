6db3f8871fcccfd94dce8d035e28979595d77506
Atrox493 <2577632081@qq.com>
2024-07-21 15:49:48 +0800

    图片懒加载指令优化

---
+++ Vue3Rabbit/src/directives/index.js
@@ -0,0 +1,18 @@
+//定义懒加载插件
+import { useIntersectionObserver } from "@vueuse/core";
+
+export const lazyPlugin = {
+  install(app) {
+    // 懒加载指令逻辑
+    app.directive("img-lazy", {
+      mounted(el, binding) {
+        // el：指令绑定的元素，binding：指令对象
+        const { stop } = useIntersectionObserver(el, ([{ isIntersecting }]) => {
+          // 进入视口区域
+          if (isIntersecting) el.src = binding.value;
+          stop();
+        });
+      },
+    });
+  },
+};
--- Vue3Rabbit/src/main.js
+++ Vue3Rabbit/src/main.js
@@ -6,21 +6,14 @@ import router from "./router";
 
 // 引入初始化的样式文件
 import "./styles/common.scss";
-import { useIntersectionObserver } from "@vueuse/core";
+
+// 引入懒加载指令插件并且注册
+import { lazyPlugin } from "./directives";
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(lazyPlugin);
 
 app.mount("#app");
-
-app.directive("img-lazy", {
-  mounted(el, binding) {
-    // el：指令绑定的元素，binding：指令对象
-    useIntersectionObserver(el, ([{ isIntersecting }]) => {
-      // 进入视口区域
-      if (isIntersecting) el.src = binding.value;
-    });
-  },
-});
