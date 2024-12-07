34f13a2b9972b368757b46a0134cfeae84ef44fd
yize <yize@2980.com>
2024-12-07 14:45:41 +0800

    二级分类-列表筛选功能实现

--- Vue3Rabbit/src/views/SubCategory/index.vue
+++ Vue3Rabbit/src/views/SubCategory/index.vue
@@ -26,6 +26,11 @@ const getGoodList = async () => {
   goodList.value = res.result.items;
 };
 
+const tabChange = () => {
+  reqData.value.page = 1;
+  getGoodList();
+};
+
 onMounted(() => {
   getCategoryData();
   getGoodList();
@@ -45,7 +50,7 @@ onMounted(() => {
       </el-breadcrumb>
     </div>
     <div class="sub-container">
-      <el-tabs>
+      <el-tabs v-model="reqData.sortField" @tab-change="tabChange">
         <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
         <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
         <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
