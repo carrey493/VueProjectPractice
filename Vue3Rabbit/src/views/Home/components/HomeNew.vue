fffb83dc40d0c4c91144c69e4d5ea33e2ddd31e0
yize <yize@2980.com>
2024-12-09 21:24:26 +0800

    详情页-整体认识与路由配置

--- Vue3Rabbit/src/views/Home/components/HomeNew.vue
+++ Vue3Rabbit/src/views/Home/components/HomeNew.vue
@@ -19,7 +19,7 @@ onMounted(() => {
   <HomePanel title="新鲜好物" sub-title="新鲜好物 品质靠谱">
     <ul class="goods-list">
       <li v-for="item in newList" :key="item.id">
-        <RouterLink to="/">
+        <RouterLink :to="`/detail/${item.id}`">
           <img :src="item.picture" alt="" />
           <p class="name">{{ item.name }}</p>
           <p class="price">&yen;{{ item.price }}</p>
