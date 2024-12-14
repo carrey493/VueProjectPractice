fb47996d05534a3abe272e04eaf376e33318efb2
yize <yize@2980.com>
2024-12-14 18:31:06 +0800

    图片预览组件-小图切换大图

--- Vue3Rabbit/src/views/Detail/index.vue
+++ Vue3Rabbit/src/views/Detail/index.vue
@@ -3,6 +3,7 @@ import { getDetail } from "@/apis/detail.js";
 import { onMounted, ref } from "vue";
 import { useRoute } from "vue-router";
 import DetailHots from "./components/DetailHots.vue";
+import ImageView from "../../components/ImageView/index.vue";
 const goods = ref({});
 const route = useRoute();
 const getGoods = async () => {
@@ -40,7 +41,7 @@ onMounted(() => getGoods());
           <div class="goods-info">
             <div class="media">
               <!-- 图片预览区 -->
-
+              <ImageView />
               <!-- 统计数量 -->
               <ul class="goods-sales">
                 <li>
