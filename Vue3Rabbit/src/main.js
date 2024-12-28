b8bc8b2d2a5f158b79a6da8a2796b9948a4a1ba0
yize <yize@2980.com>
2024-12-28 18:53:35 +0800

    详情页-全局组件插件化

--- Vue3Rabbit/src/main.js
+++ Vue3Rabbit/src/main.js
@@ -10,10 +10,13 @@ import "./styles/common.scss";
 // 引入懒加载指令插件并且注册
 import { lazyPlugin } from "./directives";
 
+import { componentPlugin } from "@/components";
+
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
 app.use(lazyPlugin);
+app.use(componentPlugin);
 
 app.mount("#app");
--- Vue3Rabbit/src/views/Detail/index.vue
+++ Vue3Rabbit/src/views/Detail/index.vue
@@ -3,8 +3,6 @@ import { getDetail } from "@/apis/detail.js";
 import { onMounted, ref } from "vue";
 import { useRoute } from "vue-router";
 import DetailHots from "./components/DetailHots.vue";
-import ImageView from "../../components/ImageView/index.vue";
-import XtxSku from "../../components/XtxSku/index.vue";
 const goods = ref({});
 const route = useRoute();
 const getGoods = async () => {
@@ -45,7 +43,7 @@ const skuChange = (sku) => {
           <div class="goods-info">
             <div class="media">
               <!-- 图片预览区 -->
-              <ImageView :imageList="goods.mainPictures" />
+              <XtxImageView :imageList="goods.mainPictures" />
               <!-- 统计数量 -->
               <ul class="goods-sales">
                 <li>
