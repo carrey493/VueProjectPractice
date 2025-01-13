4b4538fa99acc0968e086813875884fc4bdb2f67
yize <yize@2980.com>
2025-01-13 21:18:55 +0800

    登录页-pinia数据持久化

--- Vue3Rabbit/src/main.js
+++ Vue3Rabbit/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 
 import App from "./App.vue";
 import router from "./router";
@@ -14,8 +15,10 @@ import { lazyPlugin } from "./directives";
 import { componentPlugin } from "@/components";
 
 const app = createApp(App);
-
-app.use(createPinia());
+const pinia = createPinia();
+// 注册持久化插件
+pinia.use(piniaPluginPersistedstate);
+app.use(pinia);
 app.use(router);
 app.use(lazyPlugin);
 app.use(componentPlugin);
