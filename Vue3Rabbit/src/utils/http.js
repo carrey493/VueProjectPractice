6841f42dd4ed4908824b6a823d5f1c2cdd1de673
yize <yize@2980.com>
2025-01-15 21:58:34 +0800

    登录-拦截器携带token

--- Vue3Rabbit/src/utils/http.js
+++ Vue3Rabbit/src/utils/http.js
@@ -2,14 +2,22 @@
 import axios from "axios";
 import { ElMessage } from "element-plus";
 const baseUrl = "http://pcapi-xiaotuxian-front-devtest.itheima.net";
+import { useUserStore } from "@/stores/user";
+
 const httpInstance = axios.create({
   timeout: 5000,
 });
 
 // 请求拦截器
 httpInstance.interceptors.request.use(
+  // 获取token数据
   (config) => {
     config.url = baseUrl + config.url;
+    const userStore = useUserStore();
+    const token = userStore.userInfo.token;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
     return config;
   },
   (e) => Promise.reject(e)
