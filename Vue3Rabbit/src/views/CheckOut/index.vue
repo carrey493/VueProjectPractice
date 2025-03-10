1a939e5bcfe535a6aa95cbc56166adda3c87eaf9
yize <yize@2980.com>
2025-03-10 20:29:10 +0800

    结算页-地址切换

--- Vue3Rabbit/src/views/CheckOut/index.vue
+++ Vue3Rabbit/src/views/CheckOut/index.vue
@@ -13,6 +13,7 @@ const getCheckInfo = async () => {
   );
   curAddress.value = item;
 };
+const showDialog = ref(false);
 onMounted(() => {
   getCheckInfo();
 });
@@ -42,7 +43,7 @@ onMounted(() => {
               </ul>
             </div>
             <div class="action">
-              <el-button size="large" @click="toggleFlag = true"
+              <el-button size="large" @click="showDialog = true"
                 >切换地址</el-button
               >
               <el-button size="large" @click="addFlag = true"
@@ -131,6 +132,29 @@ onMounted(() => {
     </div>
   </div>
   <!-- 切换地址 -->
+  <el-dialog v-model="showDialog" title="切换收货地址" width="30%" center>
+    <div class="addressWrapper">
+      <div
+        class="text item"
+        v-for="item in checkInfo.userAddresses"
+        :key="item.id"
+      >
+        <ul>
+          <li>
+            <span>收<i />货<i />人：</span>{{ item.receiver }}
+          </li>
+          <li><span>联系方式：</span>{{ item.contact }}</li>
+          <li><span>收货地址：</span>{{ item.fullLocation + item.address }}</li>
+        </ul>
+      </div>
+    </div>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button>取消</el-button>
+        <el-button type="primary">确定</el-button>
+      </span>
+    </template>
+  </el-dialog>
   <!-- 添加地址 -->
 </template>
 
